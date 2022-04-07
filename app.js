58cb7ff196986d49515ad24d61fb1db08d5d712e
Alex Wilson <awilson.hg@gmail.com>
2022-04-06 21:33:15 -0400

    add response codes

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.get("/api/squish", (req, res) => {
 
 	if(!req.query.url)
 	{
+		res.status = 400;
 		response.error = "Missing URL parameter.";
 	}
 	else
@@ -36,6 +37,7 @@ app.get("/api/squish", (req, res) => {
 			response.error = "Not a valid URL.";
 		}
 	}
+	res.status = 200;
 	res.json(response);
 })
 
@@ -44,6 +46,7 @@ app.get("/api/:id", (req, res) => {
 	// we're using mongodb ID as url path so check length to avoid db lookup errors
 	if(req.params.id.length != 24)
 	{
+		res.status = 400;
 		res.json({ error : "couldnt find url", url : null })
 	}
 	else 
@@ -52,10 +55,12 @@ app.get("/api/:id", (req, res) => {
 		.then((data) => {
 			if(data) 
 			{
+				res.status = 200;
 				res.json({ error : null, url : data.target_url});
 			}
 			else 
 			{
+				res.status = 400;
 				res.json({ error : "couldnt find url", url : null})
 			}
 		})
@@ -68,7 +73,6 @@ app.all("*", (req, res) => {
 
 // Server Init
 
-const hostname = 'localhost';
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
